95017c5daee1d619635c0e9b119d43236833f290
Nicolas Patron <nicolas.patron@hotmail.fr>
2024-08-31 15:07:50 +0200

    ...

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -2,14 +2,18 @@ import logging
 import jwt, datetime
 from users.models import User
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 
 logger = logging.getLogger(__name__)
 class AuthenticationView(APIView):
-	def recognizeToken(self, request):
+
+	def get(self, request):
 		token = request.COOKIES.get('jwt')
 
-		return Response(token)
+		if not token:
+			raise(AuthenticationFailed)
+		return Response({"token": token})
 
 def	generateTokenJWT(id):
 
