d8c118b1acd1533aa47bf42e44c86a951de87fc2
Fabio Palumbo <fabio@MacBook-Air-de-Fabio.local>
2024-09-02 14:43:28 +0200

    changes with JWT

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, include
 from users.views import UserView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
 	path('users/', UserView.as_view()),
+	path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 	# path('create/', createUser, name='createUser'),
 ]
