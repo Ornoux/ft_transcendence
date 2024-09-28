d28906944f64e7e038b0a19b3221de995128f73d
Nicolas Patron <nicolas.patron@hotmail.fr>
2024-09-28 09:35:41 +0200

    navbar hidden in register/login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import NavbarBS from './components/Navbar.jsx';
 import GlobalGameSolo from './game_page/solo/GlobalGameSolo';
 import LoginPage from './login_page/loginPage';
@@ -10,20 +11,27 @@ import GlobalGameMulti from './game_page/multi/GlobalGameMulti';
 import RegisterPage from './register_page/registerPage';
 import './i18n';
 
+
 const App = () => {
-   return (
-     <>
-       <NavbarBS />
-       <Routes>
-         <Route path="/" element={<LoginPage />} />
-         <Route path="/home" element={<HomePage />} />
-         <Route path="/register" element={<RegisterPage />} />
-         <Route path="/ChooseGame" element={<ChooseGame />} />
-         <Route path="/GlobalGameSolo" element={<GlobalGameSolo />} />
-         <Route path="/GlobalGameMulti/:roomId" element={<GlobalGameMulti />} />
-       </Routes>
-     </>
-   );
- };
+  const location = useLocation();
+
+  const hideNavbarPaths = ['/', '/register'];
+
+  return (
+    <>
+      {!hideNavbarPaths.includes(location.pathname) && <NavbarBS />}
+      
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/ChooseGame" element={<ChooseGame />} />
+        <Route path="/GlobalGameSolo" element={<GlobalGameSolo />} />
+        <Route path="/GlobalGameMulti/:roomId" element={<GlobalGameMulti />} />
+      </Routes>
+    </>
+  );
+};
+
 
 export default App;
