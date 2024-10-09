3e3f989590efc51af68e503ff602e489c409dd83
Nicolas Patron <nicolas.patron@hotmail.fr>
2024-10-09 19:18:27 +0200

    need to end frontend chat

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -7,27 +7,86 @@ import { useAuth } from "../provider/UserAuthProvider";
 function Chat() {
 
     const {myUser} = useAuth()
+    const [friendsMessagesClicked, setFriendsMessagesClicked] = useState(false)
+    const [usersMessagesClicked, setUsersMessagesClicked] = useState(false)
 
-    const handlePlusButton = () => {
-        setPlusButton(!isPlusButtonClicked);
+    const handleFriendsMessages = () => {
+        if (friendsMessagesClicked === true) {
+            setUsersMessagesClicked(false)
+            setFriendsMessagesClicked(false)
+            return ;
+        }
+        setFriendsMessagesClicked(true)
+        if (usersMessagesClicked === true)
+            setUsersMessagesClicked(false)
+    }
+
+    const handleUsersMessages = () => {
+        if (usersMessagesClicked === true) {
+            setUsersMessagesClicked(false)
+            setFriendsMessagesClicked(false)
+            return ;
+        }
+        setUsersMessagesClicked(true)
+        if (friendsMessagesClicked === true)
+            setFriendsMessagesClicked(false)
     }
 
     return (
         <div className="chat">
 
+
+            {/* DIV USERS/FRIENDS LEFT */}
+
+
             <div className="chat-discussions">
                 <div className="chat-header">
                     <div className="header-logo logo-header">
-                        <i class="bi bi-people-fill"></i>
+                        <i onClick={handleFriendsMessages} className="bi bi-people-fill"></i>
                     </div>
                     <div className="header-logo logo-header">
-                        <i class="bi bi-chat"></i>
-                    </div>
+                        <i onClick={handleUsersMessages} className="bi bi-chat"></i>
+                    </div> 
                 </div>
+
+                {!friendsMessagesClicked && !usersMessagesClicked &&
+                <div className="welcomeMessage">
+                    <span className="welcomeMessage-span">Welcome</span>
+                    <span className="welcomeMessage-span-username">{myUser.username}</span>
+                </div>
+                }
+                {friendsMessagesClicked && !usersMessagesClicked &&
+                    <div className="welcomeMessage">
+                        <span className="welcomeMessage-span">Friends</span>
+                    </div>
+                }
+                {usersMessagesClicked && !friendsMessagesClicked &&
+                    <div className="welcomeMessage">
+                        <span className="welcomeMessage-span">USERS</span>
+                    </div>
+                }
             </div>
-            <div className="welcomeMessage">
-                <span className="welcomeMessage-span">Welcome {myUser.username}</span>
-            </div>
+
+
+                {/* DIV DISCUSSIONS RIGHT */}
+
+
+            {!friendsMessagesClicked && !usersMessagesClicked &&
+                <div className="welcomeMessage">
+                    <span className="welcomeMessage-span">Welcome</span>
+                    <span className="welcomeMessage-span-username">{myUser.username}</span>
+                </div>
+            }
+            {friendsMessagesClicked && !usersMessagesClicked &&
+                <div className="welcomeMessage">
+                    <span className="welcomeMessage-span">Friends</span>
+                </div>
+            }
+            {usersMessagesClicked && !friendsMessagesClicked &&
+                <div className="welcomeMessage">
+                    <span className="welcomeMessage-span">USERS</span>
+                </div>
+            }
         </div>
     );
 };
