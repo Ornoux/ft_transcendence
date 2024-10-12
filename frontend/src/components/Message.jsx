98216daec1171aa30155e56e27b9f96fca7a23db
Nicolas Patron <nicolas.patron@hotmail.fr>
2024-10-12 17:17:49 +0200

    end tonight

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,19 +1,22 @@
-import "./chat.css"
+function Message({ myDiscuss, myUser, userSelected }) {
 
-function Message({myDiscuss, myUser, userSelected}) {
+	const chooseCssForMePlease = (message) => {
+		if (message.sender === myUser.id)
+			return ("sender")
+		else
+			return "receiver"
+	}
 
-
-
-	return (
-	<>
-	  <div className="div-message-myUser">
-		<span className="span-message-myUser">{myDiscuss.length}</span>
-	  </div>
-	  <div className="div-message-userSelected">
-	  	<span className="span-message-myUser">{myDiscuss.length}</span>
-	  </div>
-	</>
-	);
-  }
+    return (
+        <div className="core-discussion">
+            {myDiscuss.map((message, index) => (
+                <div key={index} className={`div-message-${chooseCssForMePlease(message)}`}>
+                    <span className={`message-${chooseCssForMePlease(message)}`}></span>
+                    <span className="message-receiver">{message.message}</span>
+                </div>
+            ))}
+        </div>
+    );
+}
 
 export default Message;
